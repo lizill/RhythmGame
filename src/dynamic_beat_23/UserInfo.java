f4b0a7fe618912930e16a847cf8ac2b1e4dd87d2
Park-Donghyeon <68741593+Park-Donghyeon@users.noreply.github.com>
2021-02-04 19:27:46 +0900

    Make RhythmGame With Java - ADD Rank System

---
+++ src/dynamic_beat_23/UserInfo.java
@@ -0,0 +1,13 @@
+package dynamic_beat_23;
+
+public class UserInfo {
+
+	String userID;
+	int score;
+	
+	public UserInfo(String userID, int score) {
+		this.userID = userID;
+		this.score = score;
+	}
+	
+}
